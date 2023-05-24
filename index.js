291dec5a3353abdeb521a7a59fea2b4b886b120f
Freddie Moala <loto_toa@hotmail.com>
2023-05-24 11:23:36 +1000

    Done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // code your solution here
+
+function saturdayFun(fun = "roller-skate") {
+    return `This Saturday, I want to ${fun}!`;
+}
+
+saturdayFun('play golf');
+saturdayFun();
+
+const mondayWork = function(work = "go to the office") {
+    return `This Monday, I will ${work}.`;
+};
+
+mondayWork('play golf');
+mondayWork();
+
+function wrapAdjective(rap = "special") {
+
+
+}
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+}
+
+const encourage = wrapAdjective("!!!");
+console.log(encourage());
+console.log(encourage("amazing"));
