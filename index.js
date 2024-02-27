821aa32c72cb642126b0134e3b0df061c5794ec4
Siaosi Moala <loto_toa@hotmail.com>
2024-02-27 11:34:34 +1100

    DONE

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
-// code your solution here
+// // code your solution here
 
-function saturdayFun(fun = "roller-skate") {
-    return `This Saturday, I want to ${fun}!`;
+// function saturdayFun(fun = "roller-skate") {
+//     return `This Saturday, I want to ${fun}!`;
+// }
+
+// saturdayFun('play golf');
+// saturdayFun();
+
+// const mondayWork = function(work = "go to the office") {
+//     return `This Monday, I will ${work}.`;
+// };
+
+// mondayWork('play golf');
+// mondayWork();
+
+// function wrapAdjective(rap = "special") {
+
+
+// }
+
+// function wrapAdjective(flair = "*") {
+//     return function(adjective = "special") {
+//         return `You are ${flair}${adjective}${flair}!`;
+//     };
+// }
+
+// const encourage = wrapAdjective("!!!");
+// console.log(encourage());
+// console.log(encourage("amazing"));
+
+function saturdayFun(saturdayFun = "roller-skate"){
+    return `This Saturday, I want to ${saturdayFun}!`;
 }
 
-saturdayFun('play golf');
-saturdayFun();
-
-const mondayWork = function(work = "go to the office") {
-    return `This Monday, I will ${work}.`;
-};
-
-mondayWork('play golf');
-mondayWork();
-
-function wrapAdjective(rap = "special") {
-
+function mondayWork(mondayWork = "go to the office"){
+    return `This Monday, I will ${mondayWork}.`
 
 }
 
-function wrapAdjective(flair = "*") {
+function wrapAdjective(flare = "*"){
     return function(adjective = "special") {
-        return `You are ${flair}${adjective}${flair}!`;
-    };
-}
+        return `You are ${flare}${adjective}${flare}!`;
+    }
 
-const encourage = wrapAdjective("!!!");
-console.log(encourage());
-console.log(encourage("amazing"));
+}
